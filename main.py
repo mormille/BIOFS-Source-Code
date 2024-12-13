19f35deb9c6794e468e162729de0e5c8645028fa
mormille <lhmormille@gmail.com>
2024-12-13 14:56:04 +0900

    project structure created

---
+++ main.py
@@ -0,0 +1,44 @@
+# Main script to
+
+# importing modules for each individual component in the filtering system
+import scheduler
+import pump_type1
+import pump_type2
+import pressure_gauge
+import flowmeter
+import turbidity_sensor
+import ph_sensor
+import oxygen
+
+
+
+class biofs():
+	# CONSTRUCTOR:
+	
+	# pico 1 (main filtering system) connection
+	# pico 2 (reactor monitoring) connection
+	
+	# initialize clock
+	# instanciate scheduler (raspberry pi)
+	# instanciate pump 1 - type1 (pico1)
+	# instanciate pump 2 - type2 (pico1)
+	# instanciate pump 3 - type2 (pico2)
+	# instanciate pressure gauge 1 (pico 1)
+	# instanciate pressure gauge 2 (pico 1)
+	# instanciate flowmeter 1 (pico1)
+	# instanciate flowmeter 2 (pico1)
+	# instanciate flowmeter 3 (pico2)
+	# instanciate turbidity sensor 1 (pico2)
+	# instanciate turbidity sensor 2 (pico2)
+	# instanciate ph sensor (pico2)
+	# instancia 02 sensor (pico2)
+	
+	
+	
+
+
+def filter():
+	
+
+
+	return null
