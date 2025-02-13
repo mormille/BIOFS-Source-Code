5a77502262fbe828ac26905a2145b4ed4fdf84fa
Luiz Henrique Mormille <luiz@Luizs-MacBook-Pro.local>
2025-02-13 11:20:20 +0900

    add requirements.txt file to the Pico 1 folder

---
+++ Pico1/requirements.py
@@ -0,0 +1,14 @@
+# External dependencies for the MicroPython project
+
+# SSD1306 OLED driver (Micropython_ssd1306 by Stefan Lehmann)
+ssd1306==3.0.0
+
+# OLED library for MicroPython (Micropython_oled by Yieson Cardona)
+micropython-oled==1.13.0
+
+# Note: The following modules are part of MicroPython's firmware and
+# do not need to be installed via pip:
+# - machine
+# - time / utime
+# - onewire
+# - ds18x20
